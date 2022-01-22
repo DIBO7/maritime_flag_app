e3227fdea2eaf6338a140e365d700c29681cfdd8
theibrahim7 <ibrahim.oduola007@gmail.com>
2022-01-22 12:19:48 +0100

    installed next-pwa and configured this app to a PWA. Would still alter pwa details later

--- next.config.js
+++ next.config.js
@@ -1,3 +1,11 @@
-module.exports = {
+const withPWA = require('next-pwa')
+
+module.exports = withPWA({
   reactStrictMode: true,
-}
+  pwa:{
+  	dest: "public",
+  	disable: process.env.NODE_ENV === 'development',
+  	register: true,
+  	skipWaiting: true
+  }
+})
