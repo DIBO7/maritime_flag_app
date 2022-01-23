6c181220498fdc6a69b9047529caa7c77b3ea58f
theibrahim7 <ibrahim.oduola007@gmail.com>
2022-01-23 22:40:17 +0100

    unused files caused some errors for vercel so i deleted them

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 //npm i @popperjs/core
-import Image from "next/image";
+//import Image from "next/image";
 import Interface from "../layouts/interface";
 //import Card from "../components/card"
 import styles from '../styles/Home.module.css';
