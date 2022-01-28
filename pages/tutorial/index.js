346bcfd864251c3586e554a649496ec361d09aa8
theibrahim7 <ibrahim.oduola007@gmail.com>
2022-01-28 12:52:24 +0100

    UX improvement started with the JSX rewritten, next the CSS. previous design are in redundant.js and redundant.css

--- pages/tutorial/index.js
+++ pages/tutorial/index.js
@@ -12,38 +12,43 @@ export default function Tutorial() {
     <BareMinimumTemplate>
 
       <main style={{backgroundColor: "var(--secondary)"}}>
-        <section className={styles.pop}>          
 
-          <div className={styles.slider}>
-            <SlidingElement id="1" />
-            <SlidingElement id="2" />
-            <SlidingElement id="3" />
-            <SlidingElement id="4" />
-          </div>
+        <section>
+      {/*This section should take the entire screen (consideration should be given to browser-views)*/}
 
-          <div className={styles["nav-group"]}>
-            <Link href="#slide-1" passHref> 
-              <a> A </a> 
-            </Link>
-            <Link href="#slide-2" passHref> 
-              <a> B </a> 
-            </Link>
-            <Link href="#slide-3" passHref> 
-              <a> C </a> 
-            </Link>
-            <Link href="#slide-4" passHref> 
-              <a> D </a> 
-            </Link>
-          </div>
+            <div className={styles.slider}>
+              <SlidingElement id="1" />
+              <SlidingElement id="2" />
+              <SlidingElement id="3" />
+              <SlidingElement id="4" />
+            </div>
 
-          <footer>
+            <div className={styles["nav-group"]}>
+              <Link href="#slide-1" passHref> 
+                <a> A </a> 
+              </Link>
+              <Link href="#slide-2" passHref> 
+                <a> B </a> 
+              </Link>
+              <Link href="#slide-3" passHref> 
+                <a> C </a> 
+              </Link>
+              <Link href="#slide-4" passHref> 
+                <a> D </a> 
+              </Link>
+            </div>
+
+        </section>
+
+        <footer>
           <hr />
           <Link href="/">
             <a> Home </a>
           </Link>
+        </footer>
 
 
-          </footer>
+        <section className={styles.pop}>          
 
         </section>
     </main>
