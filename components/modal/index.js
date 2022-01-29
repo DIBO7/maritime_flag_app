294e693e0f9e3666fe094406b19395f802809d97
theibrahim7 <ibrahim.oduola007@gmail.com>
2022-01-29 17:28:51 +0100

    work still in progress..

--- components/modal/index.js
+++ components/modal/index.js
@@ -11,24 +11,23 @@ export default function ModalScreen(){
 		      </div>
 		      	
 		      	<div className="modal-body">
-				  <h5>Popover in a modal</h5>
-				  <p>This 
-				  	<a href="#" role="button" className="btn btn-secondary popover-test" title="Popover title" 
-				  		data-bs-content="Popover body content is set in this attribute.">
-				  			button
-				  	</a> 
-				  	triggers a popover on click.
-				  </p>
 				  
-				  <hr />
-				  
-				  <h5>Tooltips in a modal</h5>
-				  <p>
-				  	<a href="#" className="tooltip-test" title="Tooltip">This link</a> 
-				  	and 
-				  	<a href="#" className="tooltip-test" title="Tooltip">that link</a> 
-				  	have tooltips on hover.
-				  </p>
+				  <p>What would you like to be quizzed on?</p>
+
+				  	<div style={{textAlign: "center"}}>
+				  	<br/>
+		      		<div className="btn-group-vertical" role="group" aria-label="Basic radio toggle button group">
+					  <input type="radio" className="btn-check" name="btnradio" id="btnradio1" autoComplete="off" checked />
+					  <label className="btn btn-outline-primary" style={{backgroundColor: "var(--tertiary)"}} htmlFor="btnradio1">Names and Meaning</label>
+
+					  <input type="radio" className="btn-check" name="btnradio" id="btnradio2" autoComplete="off" />
+					  <label className="btn btn-outline-primary" htmlFor="btnradio2">Flags, Names and Meaning</label>
+
+					  <input type="radio" className="btn-check" name="btnradio" id="btnradio3" autoComplete="off" />
+					  <label className="btn btn-outline-primary" htmlFor="btnradio3">Flags, Names, Code and Meaning</label>
+					</div>
+					<br/>
+					</div>
 
 				</div>
 
@@ -37,6 +36,7 @@ export default function ModalScreen(){
 		        <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
 		        <button type="button" className="btn btn-primary">Save changes</button>
 		      </div>
+
 		    </div>
 		  </div>
 		</div>
@@ -46,6 +46,24 @@ export default function ModalScreen(){
 
 
 /*
+<p>This 
+  	<a href="#" role="button" className="btn btn-secondary popover-test" title="Popover title" 
+  		data-bs-content="Popover body content is set in this attribute.">
+  			button
+  	</a> 
+  	triggers a popover on click.
+  </p>
+  
+  <hr />
+  
+  <h5>Tooltips in a modal</h5>
+  <p>
+  	<a href="#" className="tooltip-test" title="Tooltip">This link</a> 
+  	and 
+  	<a href="#" className="tooltip-test" title="Tooltip">that link</a> 
+  	have tooltips on hover.
+  </p>
+
 The trigger ->
 
 <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
