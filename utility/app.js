c7a1a882351d2cb5eb571fe40362e0917edbe6ab
theibrahim7 <ibrahim.oduola007@gmail.com>
2022-02-20 17:19:52 +0100

    facebook and twitter meta heads adde

--- utility/app.js
+++ utility/app.js
@@ -4,6 +4,6 @@ export const appname_shortened = ""
 
 export const appname_full = "The International Maritime Signal Flag App"
 
-export const app_description = ""
+export const app_description = "Master the maritime signal flags and test your knowledge. A must use app for maritime young professionals"
 
 export const app_url = "https://maritime-flag-app.vercel.app"
