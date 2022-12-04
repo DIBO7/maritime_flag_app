db4e34c7d32d4607992ca978254df64120361f60
Ibrahim Oduola <ibrahim.oduola007@gmail.com>
2022-12-04 10:57:13 -0800

    the problem was not from the code, thankfully cos i understand the code no more. however, the problem was from the mongo database itsef and that has been rectified!

--- utility/worker.js
+++ utility/worker.js
@@ -1,4 +1,5 @@
-//const res = await axios.get("http://localhost:3000/api/v1/flags");  
+//const res = await axios.get("http://localhost:3000/api/v1/flags"); 
+//This is the live production url api to fetch flags "https://maritime-flag-app.vercel.app/api/v1/flags"
 //The getAPIData function well, gets the api data
 export const getAPIData = async function(apiURL="https://maritime-flag-app.vercel.app/api/v1/flags"){
 	const apiData = await fetch(apiURL)//for now all host (heroku, vercel and local) should fetch from vercel
